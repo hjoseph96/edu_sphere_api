85098a5a0384576bd86de08578df85572357bb46
Herbert Joseph <techniquea2z@gmail.com>
2025-09-10 12:17:24 -0400

    fix(user#index): Only search for teachers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    users = User.search(params[:query])
+    users = User.where(role: :teacher).search(params[:query])
 
     render json: { users: users.map(&:attributes) }
   end
